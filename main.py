fe6ab04f366009b3eda0e636ec78eea07946f2e2
Michael Florya <mih2303@mail.ru>
2024-04-04 13:11:00 +0300

    create database connection

--- main.py
+++ main.py
@@ -3,8 +3,22 @@ from typing import Optional, Annotated
 from fastapi import FastAPI, Depends
 from pydantic import BaseModel
 
+from contextlib import asynccontextmanager
 
-app = FastAPI()
+from database import create_tables, delete_tables
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await delete_tables()
+    print('База очищена')
+    await create_tables()
+    print('База готова к работе')
+    yield
+    print('Выключение')
+
+
+app = FastAPI(lifespan=lifespan)
 
 
 class STaskAdd(BaseModel):
