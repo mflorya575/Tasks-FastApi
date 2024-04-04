1e273c8ad7d89cda1fd027161cafbf90d359d5fc
Michael Florya <mih2303@mail.ru>
2024-04-04 13:22:45 +0300

    create file repository with code

---
+++ repository.py
@@ -0,0 +1,25 @@
+from sqlalchemy import select
+
+from database import new_session, TaskOrm
+from main import STaskAdd
+
+
+class TaskRepository:
+    @classmethod
+    async def add_one(cls, data: STaskAdd) -> int:
+        async with new_session() as session:
+            task_dict = data.model_dump()
+
+            task = TaskOrm(**task_dict)
+            session.add(task)
+            await session.flush()
+            await session.commit()
+            return task.id
+
+    @classmethod
+    async def find_all(cls):
+        async with new_session() as session:
+            query = select(TaskOrm)
+            result = await session.execute(query)
+            task_models = result.scalars().all()
+            return task_models
