79d10a11e89952fa1d1488a448a42ec2f11771b7
Michael Florya <mih2303@mail.ru>
2024-04-04 12:48:49 +0300

    add simple post page

--- main.py
+++ main.py
@@ -1,9 +1,33 @@
+from typing import Optional
+
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 
 app = FastAPI()
 
 
-@app.get('/home')
-def get_home_page():
-    return 'Hello world'
+class STaskAdd(BaseModel):
+    name: str
+    description: Optional[str] = None
+
+
+class STask(STaskAdd):
+    id: int
+
+
+tasks = []
+
+
+@app.post('/tasks')
+async def add_task(
+        task: STaskAdd,
+):
+    tasks.append(task)
+    return {'ok': True}
+
+
+# @app.get('/tasks')
+# def get_tasks():
+#     task = Task(name='Запиши это видео')
+#     return {'data': task}
